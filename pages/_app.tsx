b6c2def3c14e08246b298b3e53c76851703e3585
Franz Sinaga <sinagafranz12@gmail.com>
2022-10-13 05:40:15 +0000

    add navbar from flowbite

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Nav from "../components/NavBar/Nav";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+      </Head>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex bg-black w-full h-full flex-1 flex-col items-center justify-center bg-gradient-to-r from-amber-400 via-rose-600 to-amber-400 animate-gradient-x">
+      {/* <main className="flex bg-black w-full h-full flex-1 flex-col items-center justify-center bg-gradient-to-r from-amber-400 via-rose-600 to-amber-400 animate-gradient-x">
         <h1 className="text-6xl text-gray-100 font-bold ">
           Under {' '}
           <span className="text-green-600">
@@ -21,13 +21,10 @@ const Home: NextPage = () => {
 
         <p className="mt-3 text-gray-50 text-2xl">
           Check back soon {' '} 😅
-          {/* <code className="rounded-md bg-green-600 p-3 font-mono text-lg">
-            :)
-          </code> */}
         </p>
-      </main>
+      </main> */}
 
-      {/* <footer className="flex h-24 w-full items-center justify-center border-t">
+      <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -37,7 +34,7 @@ const Home: NextPage = () => {
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
-      </footer> */}
+      </footer>
     </div>
   )
 }
